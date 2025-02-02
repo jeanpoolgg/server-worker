49c19fad6db11b0d2efe435a8a9b56a098e02074
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-02-02 16:48:11 -0500

    feat: add fetch event

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-console.log('SW: Hola mundo 2');
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.includes("style.css")) {
+        event.respondWith(null);
+    } else {
+        event.respondWith(fetch(event.request));
+    }
+});
