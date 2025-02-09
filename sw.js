ba262c722dff478ccd845daa6ee9c37a78ed06b5
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-02-09 12:17:07 -0500

    feat: service worker install

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-self.addEventListener("fetch", (event) => {
-    
-    const resp = fetch(event.request)
-        .then( resp => {
-            return resp.ok ? resp : fetch("img/tec.avif");
-        });
 
-        event.respondWith(resp);
-    
+// Ciclo de vida del Sw
+self.addEventListener('install', event => {
+
+    // Descargar assets
+    // Creamos un cache
+    console.log('SW: Instalando SW');
 });
